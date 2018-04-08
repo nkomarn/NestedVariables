2c3f05d1857dfabf34a89bfce2d709150c588a8a
ExZiByte <exzibyte@gmail.com>
2018-04-08 07:46:33 -0500

    Create Twitter.java

---
+++ plugins/Information/com/nestedvariables/information/commands/Twitter.java
@@ -0,0 +1,23 @@
+package com.nestedvariables.information.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Twitter implements CommandExecutor{
+	
+
+	String TwitterPrefix = getConfig().getString("Prefix.Twitter");
+	Boolen TwitterEnabled = getConfig().getBoolean("Enabled.Twitter")
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		if (sender instanceof Player) {
+			player.sendMessage(ChatColor.translateAlternateColorCodes("&", TwitterPrefix));
+		}
+	}
+
+}
