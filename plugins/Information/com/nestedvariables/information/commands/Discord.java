b46fb97f33252bea5dc468314b449a4d22a3c3d8
ExZiByte <exzibyte@gmail.com>
2018-04-08 07:41:49 -0500

    Created Discord command class

---
+++ plugins/Information/com/nestedvariables/information/commands/Discord.java
@@ -0,0 +1,23 @@
+package com.nestedvariables.information.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+
+public class Discord implements CommandExecutor {
+	
+	String DiscordPrefix = getConfig().getString("prefix.Discord");
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		if (sender instanceof Player) {
+			player.sendMessage(ChatColor.translateAlternateColorCodes("&", DiscordPrefix));
+		}
+	}
+
+}
