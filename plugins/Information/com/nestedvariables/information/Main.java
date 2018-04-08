4af615ae7d26e950768712e3cc497121c13ff33a
ExZiByte <exzibyte@gmail.com>
2018-04-08 07:41:06 -0500

    Created Information Plugin
    Created Information Plugin and added code to Main.java

---
+++ plugins/Information/com/nestedvariables/information/Main.java
@@ -0,0 +1,20 @@
+package me.exzibyte.tutorial;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	public void onEnable(){
+		System.out.println("I'M UP! What I miss. ");
+		getCommand("discord").setExecutor(new Discord());
+		getCommand("twitter").setExecutor(new Twitter());
+		getCommand("facebook").setExecutor(new Facebook());
+		getCommand("instagram").setExecutor(new Instagram());
+		getCommand("info").setExecutor(new Info());
+	}
+	
+	public void onDisable() {
+		System.out.println("Night Night!");
+	}
+
+}
