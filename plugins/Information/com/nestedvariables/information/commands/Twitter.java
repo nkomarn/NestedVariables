373ab9dc52c83aacb4fb1840a45e3b94f078cbbd
ExZiByte <exzibyte@gmail.com>
2018-04-08 12:31:41 -0500

    Update Twitter.java

--- plugins/Information/com/nestedvariables/information/commands/Twitter.java
+++ plugins/Information/com/nestedvariables/information/commands/Twitter.java
@@ -6,18 +6,22 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.nestedvariables.information.Main;
+
 public class Twitter implements CommandExecutor{
 	
 
-	String TwitterPrefix = getConfig().getString("Prefix.Twitter");
-	Boolean TwitterEnabled = getConfig().getBoolean("Enabled.Twitter");
+	String TwitterPrefix = Main.plugin.getConfig().getString("Prefix.Twitter");
+	Boolean TwitterEnabled = Main.plugin.getConfig().getBoolean("Enabled.Twitter");
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player player = (Player) sender;
 		if (sender instanceof Player) {
-			player.sendMessage(ChatColor.translateAlternateColorCodes("&", TwitterPrefix));
+			player.sendMessage(ChatColor.translateAlternateColorCodes('&', TwitterPrefix));
 		}
+		return true;
 	}
 
 }
+
