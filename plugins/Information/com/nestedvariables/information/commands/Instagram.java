576d18ea2012d8301b2ecec891d4ea75cbb9003a
ExZiByte <exzibyte@gmail.com>
2018-04-08 12:31:24 -0500

    Update Instagram.java

--- plugins/Information/com/nestedvariables/information/commands/Instagram.java
+++ plugins/Information/com/nestedvariables/information/commands/Instagram.java
@@ -6,17 +6,20 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.nestedvariables.information.Main;
+
 public class Instagram implements CommandExecutor{
 	
-	String InstagramPrefix = getConfig().getString("Prefix.Instagram");
-	Boolean InstagramEnabled = getConfig().getBoolean("Enabled.Instagram");
+	String InstagramPrefix = Main.plugin.getConfig().getString("Prefix.Instagram");
+	Boolean InstagramEnabled = Main.plugin.getConfig().getBoolean("Enabled.Instagram");
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args ) {
 		Player player = (Player) sender;
 		if (sender instanceof Player) {
-			player.sendMessage(ChatColor.translateAlternateColorCodes("&", InstagramPrefix));
+			player.sendMessage(ChatColor.translateAlternateColorCodes('&', InstagramPrefix));
 		}
+		return true;
 	}
 
 }
